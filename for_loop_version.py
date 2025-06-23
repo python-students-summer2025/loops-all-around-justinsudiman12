872289845eed04b968122bcc142ac705ee512cdc
justinsudiman12 <js12894@nyu.edu>
2025-06-23 01:43:21 -0400

    Homework 8

---
+++ for_loop_version.py
@@ -0,0 +1,21 @@
+def get_starting_number():
+    while True:
+        try:
+            num = int(input("How many bottles of beer on the wall? "))
+            if num >= 1:
+                return num
+            else:
+                print("Please enter a number greater than or equal to 1.")
+        except ValueError:
+            print("Invalid input. Please enter an integer.")
+
+def sing(start):
+    for bottles in range(start, 0, -1):
+        if bottles > 1:
+            print(f"{bottles} bottles of beer on the wall, {bottles} bottles of beer.")
+            print(f"Take one down, pass it around, {bottles - 1} bottle{'s' if bottles - 1 > 1 else ''} of beer on the wall.")
+        elif bottles == 1:
+            print(f"{bottles} bottle of beer on the wall, {bottles} bottle of beer.")
+            print("Take it down, pass it around, no more bottles of beer on the wall!")
+        print()  
+        
--- main.py
+++ main.py
@@ -18,15 +18,15 @@ def main():
     - and then call the sing method, passing the number of bottles to start as its argument.
     """
 
-    # run for loop version
+    # # # run for loop version
     num_bottles = flv.get_starting_number()
     flv.sing(num_bottles)
 
-    # run while loop with accumulator version
+    # # # run while loop with accumulator version
     num_bottles = wlav.get_starting_number()
     wlav.sing(num_bottles)
 
-    # run while loop with flag version
+    # # # run while loop with flag version
     num_bottles = wlfv.get_starting_number()
     wlfv.sing(num_bottles)
 
